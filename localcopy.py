0cb9a25c80ca5baa6acb28161a5128d09fe0122a
sakawejsza <63130114+sakawejsza@users.noreply.github.com>
2020-09-18 15:36:00 -0500

    Update localcopy.py

--- localcopy.py
+++ localcopy.py
@@ -1,7 +1,7 @@
 from urllib.request import urlretrieve
 
 URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-LOCAL_FILE = 'c:/Users/sakaw/Downloads/TCMG 412/exirlocal_copy.txt'
+LOCAL_FILE = './local_copy.txt'
 
 # Use urlretrieve() to fetch a remote copy and save into the local file path
 local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
