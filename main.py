bc1a41761498433e64e8485bf27f848e522cc651
sakawejsza <63130114+sakawejsza@users.noreply.github.com>
2020-09-18 01:45:03 -0500

    creating list of dates

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ beginyear = '12/Oct/1994'
 beginmonth = 'Oct/1994'
 endyear = '12/Oct/1995'
 clean=[]
+
+#put line into list
+elements = line.split()
+#collect date data
+date = elements[3]
+#clean up date
+clean.append(date[1:12])
+# have a list named clean full of dates day/month/year
+
