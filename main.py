50a56ffdb374d1da9084bf63ddf30541e4e92ddd
sakawejsza <63130114+sakawejsza@users.noreply.github.com>
2020-09-18 10:01:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,29 +23,29 @@ endyear = '12/Oct/1995'
 # finding the number of orders in the last year 
 
 #finding the start of the year
-startyear = count(beginyear)
-if startyear = 0
+startyear = clean.count(beginyear)
+if startyear == 0:
   start = 0
-if count > 0
+if startyear > 0:
   for x in clean:
     simpdate = x[3:11]
-    if simpdate == 'Oct/1994'
+    if simpdate == 'Oct/1994':
       start = (clean.index(x))
       break
       
 #finding the end of the year      
-finishyear = count(endyear)
-if finishyear > 1
+finishyear = clean.count(endyear)
+if finishyear > 1:
   for x in clean:
-    if x == endyear
+    if x == endyear:
       end = (clean.index(x))
       break
-else
-  end = clean.length()
+else:
+  end = len(clean)
   
 #determining number of orders total and last year  
-total_log = clean.length()
+total_log = len(clean)
 total_year = end - start
         
-print( total_log)
+print(total_log)
 print(total_year)
