45b8885a3fe77d67188432c1a2ac64e95a216251
sakawejsza <63130114+sakawejsza@users.noreply.github.com>
2020-09-18 02:24:36 -0500

    Comments and Outputs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-beginyear = '12/Oct/1994'
-beginmonth = 'Oct/1994'
-endyear = '12/Oct/1995'
-clean=[]
+beginyear = '12/Oct/1994' #beginning of year
+beginmonth = 'Oct/1994' #beginning month
+endyear = '12/Oct/1995' #end of year
+clean=[] #creating a list for all the dates
 
 
 
@@ -16,6 +16,7 @@ clean=[]
 
 #finding the start of the year
 startyear = count(beginyear)
+#if the beginning of the year doesn't appear in the list then start on the first avalible date of october 1994
 if startyear = 0
   start = 0
 if count > 0
@@ -27,15 +28,20 @@ if count > 0
       
 #finding the end of the year      
 finishyear = count(endyear)
-if finishyear > 1
+#if end of year exists find the index and subtract 1 so it ends at a year
+if finishyear > 0
   for x in clean:
     if x == endyear
-      end = (clean.index(x))
+      end = (clean.index(x)-1)
       break
+ #if end does not exist then the end of the year is just the end of the file
 else
   end = clean.length()
   
 #determining number of orders total and last year  
 total_log = clean.length()
 total_year = end - start
+
+print('There were ', total_year,' amount of orders in the last year')
+print('There were ', total_log,' orders in the log')
         
